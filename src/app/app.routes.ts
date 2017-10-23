5bbd57053e68128faf53919a33d3de2699a18950
Nicolas Bloj <nicolasbloj@gmail.com>
2017-10-23 10:44:21 -0300

    clase128 separar rutas hijas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,14 @@
+import { USUARIO_ROUTES } from './components/usuario/usuario.routes';
 import { RouterModule, Routes } from '@angular/router';
 
-import { EditarUsuarioComponent } from './components/usuario/editar-usuario.component';
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
-import { NuevoUsuarioComponent } from './components/usuario/nuevo-usuario.component';
-import { DetallarUsuarioComponent } from './components/usuario/detallar-usuario.component';
 
 const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
     {
         path: 'usuario/:id', component: UsuarioComponent,
-        children: [
-            { path: 'nuevo', component: NuevoUsuarioComponent },
-            { path: 'editar', component: EditarUsuarioComponent },
-            { path: 'detallar', component: DetallarUsuarioComponent },
-            { path: '**', pathMatch: 'full', redirectTo: 'nuevo' }
-        ]
+        children: USUARIO_ROUTES
     },
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
     // Si no puede machear ninguna de las anteriores
---
+++ src/app/components/usuario/usuario.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+
+import { DetallarUsuarioComponent } from './detallar-usuario.component';
+import { EditarUsuarioComponent } from './editar-usuario.component';
+import { NuevoUsuarioComponent } from './nuevo-usuario.component';
+
+
+export const USUARIO_ROUTES: Routes = [
+    { path: 'nuevo', component: NuevoUsuarioComponent },
+    { path: 'editar', component: EditarUsuarioComponent },
+    { path: 'detallar', component: DetallarUsuarioComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'nuevo' }
+];
